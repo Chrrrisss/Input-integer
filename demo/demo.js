32a57b87d03ee7090de1bd82953e5ad34d717f94
Chrrrisss <chrisayodele10@gmail.com>
2022-07-29 02:16:36 +0100

    Add bundle.js

---
+++ demo/demo.js
@@ -0,0 +1,5 @@
+const input_Integer = require('..')
+
+const x = input_Integer()
+
+document.body.innerHTML = `<h1> ${x} </h1>`
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = input_Integer
+
+function input_Integer () {
+    const name = "Football Academy"
+    return name
+}
